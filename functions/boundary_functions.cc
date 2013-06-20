920f05c7f0b4855f6c1a8e2c1acbcc38583363e9
Kammer David Simon <david.kammer@epfl.ch>
2013-06-20 21:46:55 +0000

    take out stupid try catch

--- functions/boundary_functions.cc
+++ functions/boundary_functions.cc
@@ -11,20 +11,21 @@ Real integrateResidual(const std::string & sub_boundary_name,
   const Mesh & mesh = model.getMesh();
   const Array<Real> & residual = model.getResidual();
 
-  try {
-    const SubBoundary & boundary = mesh.getSubBoundary(sub_boundary_name);
-    SubBoundary::nodes_const_iterator nit  = boundary.nodes_begin();
-    SubBoundary::nodes_const_iterator nend = boundary.nodes_end();
-    for (; nit != nend; ++nit) {
-      bool is_local_node = mesh.isLocalOrMasterNode(*nit);
-      if (is_local_node) {
-	int_res += residual(*nit, dir);
-      }
+  // do not need try catch, as all subboundaries should be everywhere. 
+  //  try {
+  const SubBoundary & boundary = mesh.getSubBoundary(sub_boundary_name);
+  SubBoundary::nodes_const_iterator nit  = boundary.nodes_begin();
+  SubBoundary::nodes_const_iterator nend = boundary.nodes_end();
+  for (; nit != nend; ++nit) {
+    bool is_local_node = mesh.isLocalOrMasterNode(*nit);
+    if (is_local_node) {
+      int_res += residual(*nit, dir);
     }
-  } catch(debug::Exception e) {
-    // AKANTU_DEBUG_ERROR("Error computing integrateResidual. Cannot get SubBoundary: " 
-    // 		       << sub_boundary_name << " [" << e.what() << "]");
   }
+  // } catch(debug::Exception e) {
+  //   // AKANTU_DEBUG_ERROR("Error computing integrateResidual. Cannot get SubBoundary: " 
+  //   // 		       << sub_boundary_name << " [" << e.what() << "]");
+  // }
 
   StaticCommunicator::getStaticCommunicator().allReduce(&int_res, 1, _so_sum);
   return int_res;
