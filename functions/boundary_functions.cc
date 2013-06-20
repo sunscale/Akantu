f7e92e6fb3e9141636ff5821a2cb5992204ac81c
Kammer David Simon <david.kammer@epfl.ch>
2013-06-20 16:57:28 +0000

    add try catch to integrateResidual

--- functions/boundary_functions.cc
+++ functions/boundary_functions.cc
@@ -11,14 +11,19 @@ Real integrateResidual(const std::string & sub_boundary_name,
   const Mesh & mesh = model.getMesh();
   const Array<Real> & residual = model.getResidual();
 
-  const SubBoundary & boundary = mesh.getSubBoundary(sub_boundary_name);
-  SubBoundary::nodes_const_iterator nit  = boundary.nodes_begin();
-  SubBoundary::nodes_const_iterator nend = boundary.nodes_end();
-  for (; nit != nend; ++nit) {
-    bool is_local_node = mesh.isLocalOrMasterNode(*nit);
-    if (is_local_node) {
-      int_res += residual(*nit, dir);
+  try {
+    const SubBoundary & boundary = mesh.getSubBoundary(sub_boundary_name);
+    SubBoundary::nodes_const_iterator nit  = boundary.nodes_begin();
+    SubBoundary::nodes_const_iterator nend = boundary.nodes_end();
+    for (; nit != nend; ++nit) {
+      bool is_local_node = mesh.isLocalOrMasterNode(*nit);
+      if (is_local_node) {
+	int_res += residual(*nit, dir);
+      }
     }
+  } catch(debug::Exception e) {
+    // AKANTU_DEBUG_ERROR("Error computing integrateResidual. Cannot get SubBoundary: " 
+    // 		       << sub_boundary_name << " [" << e.what() << "]");
   }
 
   StaticCommunicator::getStaticCommunicator().allReduce(&int_res, 1, _so_sum);
