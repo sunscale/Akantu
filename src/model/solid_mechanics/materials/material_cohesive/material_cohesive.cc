37ee2b02c7e3d241e84d3b9dc40cd0468837a568
Marco Vocialta <marco.vocialta@epfl.ch>
2015-03-02 18:13:46 +0100

    Reverting a small modification added for debugging

--- src/model/solid_mechanics/materials/material_cohesive/material_cohesive.cc
+++ src/model/solid_mechanics/materials/material_cohesive/material_cohesive.cc
@@ -486,16 +486,11 @@ void MaterialCohesive::computeEnergies() {
     Array<Real>::vector_iterator traction_end =
       tractions(*it, _not_ghost).end(spatial_dimension);
 
-    // /// loop on each quadrature point
-    // for (; traction_it != traction_end;
-    // 	 ++traction_it, ++traction_old_it,
-    // 	   ++opening_it, ++opening_old_it,
-    // 	   ++erev, ++etot) {
-
-    for (UInt q = 0; traction_it != traction_end;
-	 ++traction_it, ++traction_old_it,
-	   ++opening_it, ++opening_old_it,
-	   ++erev, ++etot, ++q) {
+    /// loop on each quadrature point
+    for (; traction_it != traction_end;
+    	 ++traction_it, ++traction_old_it,
+    	   ++opening_it, ++opening_old_it,
+    	   ++erev, ++etot) {
 
       /// trapezoidal integration
       b  = *opening_it;
