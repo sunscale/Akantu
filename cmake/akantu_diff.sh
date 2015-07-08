4faaebe44a134b4f6b487aa557cd3c5a0f975605
Guillaume Anciaux <guillaume.anciaux@epfl.ch>
2015-07-08 14:37:32 +0200

    try to adapt the verification script

--- cmake/akantu_diff.sh
+++ cmake/akantu_diff.sh
@@ -6,6 +6,8 @@ ret=$?
 
 if [ $ret -eq 0 ]
 then
+    cat $1.lastout | tr '\n' '\r' | sed "s/--------------------------------------------------------------------------\r\[\[[0-9]\+,[0-9]\+\],[0-9]\+\]: A high-performance Open MPI point-to-point messaging module\rwas unable to find any relevant network interfaces:\r\rModule: OpenFabrics (openib)\r  Host: scratch\r\rAnother transport will be used instead, although this may result in\rlower performance.\r--------------------------------------------------------------------------\r//" | tr '\r' '\n' > $1.lastout.replacement
+    cp $1.lastout.replacement $1.lastout
     diff -q $1.lastout $2 && echo "Test passed!!!"
 else
     echo "Test Failed!!"
