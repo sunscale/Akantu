17f87b17c1ed07db5e7c2cc3915c9e2c979d5e70
Clement Roux <clement.roux@epfl.ch>
2015-09-28 18:30:15 +0200

    split geometrical operation and igfem mesh generation

--- src/mesh_igfem_spherical_growing_gel.hh
+++ src/mesh_igfem_spherical_growing_gel.hh
@@ -190,7 +190,7 @@ public:
   void buildResultFromQueryList(const std::list<SK::Sphere_3> & query_list) {
     /// store number of currently enriched nodes
     this->nb_enriched_nodes = mesh.getNbNodes() - nb_nodes_fem;
-   constructData();  
+    constructData();  
     //Solution 1
     //     it = mesh.firstType();
     //     end = mesh.lastType();
@@ -218,11 +218,285 @@ public:
     std::list<SK::Sphere_3> sphere_list;
     for (; query_it != query_end ; ++query_it) {
       SK::Sphere_3 sphere(query_it->center(),
-			    query_it->squared_radius() * inf_fact * inf_fact );
+			  query_it->squared_radius() * inf_fact * inf_fact );
       sphere_list.push_back(sphere);
     }
     buildResultFromQueryList(sphere_list);
   }
+
+  /// Compute the intersection points between the mesh and the query list for all element types and send the NewNodeEvent
+  void computeMeshQueryListIntersectionPoint(const std::list<SK::Sphere_3> & query_list) {
+    /// store number of currently enriched nodes
+    this->nb_enriched_nodes = mesh.getNbNodes() - nb_nodes_fem;
+    Array<Real> new_nodes(0,dim); // new nodes from intersection of all types
+    constructData();  
+    
+    std::map<ElementType, MeshAbstractIntersector<SK::Sphere_3> *>::iterator iit
+      = intersectors.begin();
+    std::map<ElementType, MeshAbstractIntersector<SK::Sphere_3> *>::iterator iend
+      = intersectors.end();
+    for(;iit != iend; ++iit) {
+      MeshAbstractIntersector<SK::Sphere_3> & intersector = *(iit->second);
+      intersector.computeMeshQueryListIntersectionPoint(query_list);
+      const Array<Real> new_node_current_type = *(intersector.getNewNodes());
+      
+      /// Send the new node event
+      if(new_node_current_type.getSize()) {
+	Array<Real> & nodes = this->mesh.getNodes();
+	UInt nb_node = nodes.getSize() ;
+	NewNodesEvent new_nodes_event;
+	for(UInt in = 0; in < new_node_current_type.getSize(); ++in ) {
+	  Vector<Real> new_node(dim, 0.0);
+	  for(UInt id = 0; id < dim; ++id)
+	    new_node(id) = new_node_current_type(in,id);
+	  nodes.push_back(new_node);
+	  new_nodes_event.getList().push_back(nb_node);
+	  ++nb_node;
+	}
+	this->mesh.sendEvent(new_nodes_event);
+      }
+    }
+    
+    //removeAdditionnalNodes();
+    ///MeshUtils::purifyMesh(mesh);
+  }
+
+/// increase sphere radius and build the new intersection points between the mesh and the query list for all element types and send the NewNodeEvent
+  void computeMeshQueryListIntersectionPoint(const std::list<SK::Sphere_3> & query_list, Real inf_fact) {
+    std::list<SK::Sphere_3>::const_iterator query_it = query_list.begin(),
+      query_end = query_list.end();
+    std::list<SK::Sphere_3> sphere_list;
+    for (; query_it != query_end ; ++query_it) {
+      SK::Sphere_3 sphere(query_it->center(),
+			  query_it->squared_radius() * inf_fact * inf_fact );
+      sphere_list.push_back(sphere);
+    }
+    computeMeshQueryListIntersectionPoint(sphere_list);
+  }
+
+  /// Build the IGFEM mesh from intersection points
+  void buildIgfemMesh(){
+    AKANTU_DEBUG_IN();
+
+    std::map<ElementType, MeshAbstractIntersector<SK::Sphere_3> *>::iterator iit
+      = intersectors.begin();
+    std::map<ElementType, MeshAbstractIntersector<SK::Sphere_3> *>::iterator iend
+      = intersectors.end();
+    for(;iit != iend; ++iit) {
+      MeshAbstractIntersector<SK::Sphere_3> & intersector = *(iit->second);
+      const Array<UInt> new_node_per_elem = *(intersector.getNewNodePerElem());
+      const UInt nb_prim_by_el = intersector.getNbSegByEl();
+
+      ElementType type = iit->first;
+      if( (type !=_triangle_3) && (type !=_igfem_triangle_4) && (type !=_igfem_triangle_5) ) {
+	AKANTU_DEBUG_ERROR("Not ready for mesh type " << type);
+      }
+
+      Array<UInt> & connec_type_tmpl = this->mesh.getConnectivity(type);
+
+      Array<UInt>
+	& connec_igfem_tri4 = this->mesh.getConnectivity(_igfem_triangle_4),
+	& connec_igfem_tri5 = this->mesh.getConnectivity(_igfem_triangle_5),
+	& connec_tri3 = this->mesh.getConnectivity(_triangle_3);
+
+      Element element_tri3(_triangle_3, 0, _not_ghost, _ek_regular);
+      Element element_tri4(_igfem_triangle_4, 0, _not_ghost, _ek_igfem);
+      Element element_tri5(_igfem_triangle_5, 0, _not_ghost, _ek_igfem);
+
+      NewIGFEMElementsEvent new_elements;
+      UInt n_new_el = 0;
+      Array<Element> & new_elements_list = new_elements.getList();
+      Array<Element> & old_elements_list = new_elements.getOldElementsList();
+ 
+      RemovedElementsEvent remove_elem(this->mesh);
+      remove_elem.getNewNumbering().alloc(connec_type_tmpl.getSize(), 1, type, _not_ghost);
+      Array<UInt> & new_numbering = remove_elem.getNewNumbering(type, _not_ghost);
+      UInt new_nb_type_tmpl = 0; // Meter of the number of element (type) will we loop on elements
+
+      for (UInt nel = 0 ; nel != new_node_per_elem.getSize(); ++nel) {
+	if( (type != _triangle_3)
+	    && ( (new_node_per_elem(nel,0)==0)
+		 || ( (new_node_per_elem(nel,0) == 1) 
+		      && ( ( (new_node_per_elem(nel,2)+1) % nb_prim_by_el ) 
+			   != new_node_per_elem(nel, new_node_per_elem.getNbComponent() - 2) ) ) ) ){
+	  Element element_type_tmpl(type, 0, _not_ghost, Mesh::getKind(type));  
+	  new_elements_list.resize(n_new_el+1);
+	  old_elements_list.resize(n_new_el+1);
+	  Vector<UInt> ctri3(3);
+	  ctri3(0) =  connec_type_tmpl(nel,0);
+	  ctri3(1) =  connec_type_tmpl(nel,1);
+	  ctri3(2) =  connec_type_tmpl(nel,2);
+	  /// add the new element in the mesh
+	  UInt nb_tri3 = connec_tri3.getSize();
+	  connec_tri3.push_back(ctri3);
+	  element_tri3.element = nb_tri3;
+	  new_elements_list(n_new_el) = element_tri3;
+	  /// the number of the old element in the mesh
+	  element_type_tmpl.element = nel;
+	  old_elements_list(n_new_el) = element_type_tmpl;
+	  new_numbering(nel) =  UInt(-1);
+	  ++n_new_el;
+
+	  remove_elem.getList().push_back(element_type_tmpl);
+  
+	}
+	else if( (new_node_per_elem(nel,0)!=0)
+		 && !( (new_node_per_elem(nel,0) == 1) 
+		       && ( ( (new_node_per_elem(nel,2)+1) % nb_prim_by_el ) 
+			    != new_node_per_elem(nel, new_node_per_elem.getNbComponent() - 2) ) ) ){
+	  Element element_type_tmpl(type, 0, _not_ghost);
+	  element_type_tmpl.kind = Mesh::getKind(type);
+	  new_elements_list.resize(n_new_el+1);
+	  old_elements_list.resize(n_new_el+1);
+	  switch(new_node_per_elem(nel,0)){
+	  case 1 :{
+	    Vector<UInt> ctri4(4);
+	    switch(new_node_per_elem(nel,2)){
+	    case 1 :
+	      ctri4(0) = connec_type_tmpl(nel,2);
+	      ctri4(1) = connec_type_tmpl(nel,0);
+	      ctri4(2) = connec_type_tmpl(nel,1);
+	      break;
+	    case 2 :
+	      ctri4(0) = connec_type_tmpl(nel,0);
+	      ctri4(1) = connec_type_tmpl(nel,1);
+	      ctri4(2) = connec_type_tmpl(nel,2);
+	      break;
+	    case 3 :
+	      ctri4(0) = connec_type_tmpl(nel,1);
+	      ctri4(1) = connec_type_tmpl(nel,2);
+	      ctri4(2) = connec_type_tmpl(nel,0);
+	      break;
+	    default :
+	      AKANTU_DEBUG_ERROR("A triangle have only 3 segments not "<< new_node_per_elem(nel,2));
+	      break;
+	    }
+	    ctri4(3) = new_node_per_elem(nel,1);
+	    UInt nb_tri4 = connec_igfem_tri4.getSize();
+	    connec_igfem_tri4.push_back(ctri4);
+	    element_tri4.element = nb_tri4;
+	    //new_elements.getList().push_back(element_tri4);
+	    new_elements_list(n_new_el) = element_tri4;
+	    if(type == _igfem_triangle_4)
+	      new_numbering.push_back(connec_igfem_tri4.getSize()-2);
+	    break;
+	  }
+	  case 2 :{
+	    Vector<UInt> ctri5(5);
+	    if( (new_node_per_elem(nel,2)==1) && (new_node_per_elem(nel,4)==2) ){
+	      ctri5(0) = connec_type_tmpl(nel,1);
+	      ctri5(1) = connec_type_tmpl(nel,2);
+	      ctri5(2) = connec_type_tmpl(nel,0);
+	      ctri5(3) = new_node_per_elem(nel,3);
+	      ctri5(4) = new_node_per_elem(nel,1);
+	    }
+	    else if((new_node_per_elem(nel,2)==1) && (new_node_per_elem(nel,4)==3)){
+	      ctri5(0) = connec_type_tmpl(nel,0);
+	      ctri5(1) = connec_type_tmpl(nel,1);
+	      ctri5(2) = connec_type_tmpl(nel,2);
+	      ctri5(3) = new_node_per_elem(nel,1);
+	      ctri5(4) = new_node_per_elem(nel,3);
+	    }
+	    else if((new_node_per_elem(nel,2)==2) && (new_node_per_elem(nel,4)==3)){
+	      ctri5(0) = connec_type_tmpl(nel,2);
+	      ctri5(1) = connec_type_tmpl(nel,0);
+	      ctri5(2) = connec_type_tmpl(nel,1);
+	      ctri5(3) = new_node_per_elem(nel,3);
+	      ctri5(4) = new_node_per_elem(nel,1);
+	    }
+	    else if((new_node_per_elem(nel,2)==2) && (new_node_per_elem(nel,4)==1)){
+	      ctri5(0) = connec_type_tmpl(nel,1);
+	      ctri5(1) = connec_type_tmpl(nel,2);
+	      ctri5(2) = connec_type_tmpl(nel,0);
+	      ctri5(3) = new_node_per_elem(nel,1);
+	      ctri5(4) = new_node_per_elem(nel,3);
+	    }
+	    else if((new_node_per_elem(nel,2)==3) && (new_node_per_elem(nel,4)==1)){
+	      ctri5(0) = connec_type_tmpl(nel,0);
+	      ctri5(1) = connec_type_tmpl(nel,1);
+	      ctri5(2) = connec_type_tmpl(nel,2);
+	      ctri5(3) = new_node_per_elem(nel,3);
+	      ctri5(4) = new_node_per_elem(nel,1);
+	    }
+	    else if((new_node_per_elem(nel,2)==3) && (new_node_per_elem(nel,4)==2)){
+	      ctri5(0) = connec_type_tmpl(nel,2);
+	      ctri5(1) = connec_type_tmpl(nel,0);
+	      ctri5(2) = connec_type_tmpl(nel,1);
+	      ctri5(3) = new_node_per_elem(nel,1);
+	      ctri5(4) = new_node_per_elem(nel,3);
+	    }
+	    else{
+	      AKANTU_DEBUG_ERROR("A triangle have only 3 segments not "<< new_node_per_elem(nel,2) << "and" << new_node_per_elem(nel,4));
+	    }	      
+	    UInt nb_tri5 = connec_igfem_tri5.getSize();
+	    connec_igfem_tri5.push_back(ctri5);
+	    element_tri5.element = nb_tri5;
+	    //new_elements.getList().push_back(element_tri5);
+	    new_elements_list(n_new_el) = element_tri5;
+	    if(type == _igfem_triangle_5){
+	      new_numbering.push_back(new_nb_type_tmpl);
+	      ++new_nb_type_tmpl;
+	    }
+	    break;
+	  }
+	  default:
+	    AKANTU_DEBUG_ERROR("Igfem cannot add "<< new_node_per_elem(nel,0) << " nodes to triangles");
+	    break;
+	  }
+	  element_type_tmpl.element = nel;
+	  old_elements_list(n_new_el) = element_type_tmpl;
+	  remove_elem.getList().push_back(element_type_tmpl);
+	  new_numbering(nel) =  UInt(-1);
+	  ++n_new_el;
+	}
+	else{
+	  new_numbering(nel) = new_nb_type_tmpl;
+	  ++new_nb_type_tmpl;
+	}
+      }
+
+      // for(UInt nel=new_node_per_elem.getSize(); nel < this->mesh.getNbElement(type); ++nel) {
+      //     new_numbering(nel) = new_nb_type_tmpl;
+      //     ++new_nb_type_tmpl;
+      // }
+
+      UInt el_index = 0;
+      for (UInt e = 0; e < this->mesh.getNbElement(type); ++e) {
+	if (new_numbering(e) != UInt(-1)) {
+	  new_numbering(e) = el_index;
+	  ++el_index;
+	}
+      }
+    
+      if(n_new_el > 0){
+	this->mesh.sendEvent(new_elements);
+	this->mesh.sendEvent(remove_elem);
+      }
+    }
+
+    removeAdditionnalNodes();
+    AKANTU_DEBUG_OUT();
+    }
+
+  /// Build the IGFEM mesh from spheres
+  void buildIgfemMeshFromSpheres(const std::list<SK::Sphere_3> & query_list){
+    AKANTU_DEBUG_IN();
+    
+    computeMeshQueryListIntersectionPoint(query_list);
+    buildIgfemMesh();
+
+    AKANTU_DEBUG_OUT();
+    }
+
+  /// Build the IGFEM mesh from spheres with increase factor
+  void buildIgfemMeshFromSpheres(const std::list<SK::Sphere_3> & query_list, Real inf_fact){
+    AKANTU_DEBUG_IN();
+    
+    computeMeshQueryListIntersectionPoint(query_list, inf_fact);
+    buildIgfemMesh();
+
+    AKANTU_DEBUG_OUT();
+    }
 
 protected:
   /// Mesh used to construct the primitives
--- test/test_igfem/test_generation_igfem_mesh_from_sphere.cc
+++ test/test_igfem/test_generation_igfem_mesh_from_sphere.cc
@@ -196,10 +196,95 @@ int main (int argc, char * argv[]) {
   nb_tri4_gel = mesh_gel.getConnectivity(_igfem_triangle_4).getSize();
   nb_tri5_gel = mesh_gel.getConnectivity(_igfem_triangle_5).getSize();
   if ( (nb_tri3_gel != 1) || (nb_tri4_gel != 0) || (nb_tri5_gel != 1)){ 
-    std::cout << "final mesh with " << nb_tri3_gel << " _triangle_3, and " << nb_tri4_gel
+    std::cout << "mesh at 3.5 with " << nb_tri3_gel << " _triangle_3, and " << nb_tri4_gel
 	      << " _igfem_triangle_4, and " << nb_tri5_gel << " _igfem_triangle_5"<< std::endl; 
     return EXIT_FAILURE;
     }
+
+  /// test of new MeshIgfemSphericalGrowingGel
+  Mesh mesh_gel2(2, "mesh_gel2");
+  mesh_gel2.read("test_geometry_triangle.msh");
+  MeshIgfemSphericalGrowingGel<2> gel_intersector2(mesh_gel2);
+
+  SK::Sphere_3 sphere_gel2(SK::Point_3(1, 0, 0), 0.4999999999/4);
+  std::list<SK::Sphere_3> sphere_list_gel2;
+  sphere_list_gel2.push_back(sphere_gel2);
+
+  DumperParaview dumper_gel2_igfem("mesh_gel2_igfem");
+  dumper_gel2_igfem.registerMesh(mesh_gel2, 2, _not_ghost, _ek_igfem);
+  DumperParaview dumper_gel2_regular("mesh_gel2_regular");
+  dumper_gel2_regular.registerMesh(mesh_gel2, 2, _not_ghost, _ek_regular);
+  dumper_gel2_regular.dump();
+
+  gel_intersector2.buildIgfemMeshFromSpheres(sphere_list_gel2);
+  //gel_intersector2.computeMeshQueryListIntersectionPoint(sphere_list_gel2);
+  //gel_intersector2.buildIgfemMesh();
+  dumper_gel2_igfem.dump();
+  dumper_gel2_regular.dump();
+  UInt nb_tri3_gel2 = mesh_gel2.getConnectivity(_triangle_3).getSize();
+  UInt nb_tri4_gel2 = mesh_gel2.getConnectivity(_igfem_triangle_4).getSize();
+  UInt nb_tri5_gel2 = mesh_gel2.getConnectivity(_igfem_triangle_5).getSize();
+  if ( (nb_tri3_gel2 != 1) || (nb_tri4_gel2 != 0) || (nb_tri5_gel2 != 1)){ 
+    std::cout << "interm. mesh_gel with " << nb_tri3_gel2 << " _triangle_3, and " << nb_tri4_gel2
+	      << " _igfem_triangle_4, and " << nb_tri5_gel2 << " _igfem_triangle_5"<< std::endl; 
+    return EXIT_FAILURE;
+  }
+
+  gel_intersector2.buildIgfemMeshFromSpheres(sphere_list_gel2, 1.5);
+  //gel_intersector2.computeMeshQueryListIntersectionPoint(sphere_list_gel2, 1.5);
+  //gel_intersector2.buildIgfemMesh();
+  dumper_gel2_igfem.dump();
+  dumper_gel2_regular.dump();
+  nb_tri3_gel2 = mesh_gel2.getConnectivity(_triangle_3).getSize();
+  nb_tri4_gel2 = mesh_gel2.getConnectivity(_igfem_triangle_4).getSize();
+  nb_tri5_gel2 = mesh_gel2.getConnectivity(_igfem_triangle_5).getSize();
+  if ( (nb_tri3_gel2 != 1) || (nb_tri4_gel2 != 0) || (nb_tri5_gel2 != 1)){ 
+    std::cout << "final mesh with " << nb_tri3_gel2 << " _triangle_3, and " << nb_tri4_gel2
+	      << " _igfem_triangle_4, and " << nb_tri5_gel2 << " _igfem_triangle_5"<< std::endl; 
+    return EXIT_FAILURE;
+    }
+
+  gel_intersector2.buildIgfemMeshFromSpheres(sphere_list_gel2, 2);
+  //gel_intersector2.computeMeshQueryListIntersectionPoint(sphere_list_gel2, 2);
+  //gel_intersector2.buildIgfemMesh();
+  dumper_gel2_igfem.dump();
+  dumper_gel2_regular.dump();
+  nb_tri3_gel2 = mesh_gel2.getConnectivity(_triangle_3).getSize();
+  nb_tri4_gel2 = mesh_gel2.getConnectivity(_igfem_triangle_4).getSize();
+  nb_tri5_gel2 = mesh_gel2.getConnectivity(_igfem_triangle_5).getSize();
+  if ( (nb_tri3_gel2 != 1) || (nb_tri4_gel2 != 1) || (nb_tri5_gel2 != 0)){ 
+    std::cout << "final mesh with " << nb_tri3_gel2 << " _triangle_3, and " << nb_tri4_gel2
+	      << " _igfem_triangle_4, and " << nb_tri5_gel2 << " _igfem_triangle_5"<< std::endl; 
+    return EXIT_FAILURE;
+    }
+
+  gel_intersector2.buildIgfemMeshFromSpheres(sphere_list_gel2,2.1);
+  //gel_intersector2.computeMeshQueryListIntersectionPoint(sphere_list_gel2,2.1);
+  //gel_intersector2.buildIgfemMesh();
+  dumper_gel2_igfem.dump();
+  dumper_gel2_regular.dump();
+  nb_tri3_gel2 = mesh_gel2.getConnectivity(_triangle_3).getSize();
+  nb_tri4_gel2 = mesh_gel2.getConnectivity(_igfem_triangle_4).getSize();
+  nb_tri5_gel2 = mesh_gel2.getConnectivity(_igfem_triangle_5).getSize();
+  if ( (nb_tri3_gel2 != 0) || (nb_tri4_gel2 != 0) || (nb_tri5_gel2 != 2)){ 
+    std::cout << "final mesh with " << nb_tri3_gel2 << " _triangle_3, and " << nb_tri4_gel2
+	      << " _igfem_triangle_4, and " << nb_tri5_gel2 << " _igfem_triangle_5"<< std::endl; 
+    return EXIT_FAILURE;
+    }
+  
+  gel_intersector2.buildIgfemMeshFromSpheres(sphere_list_gel,3.5);
+  //gel_intersector2.computeMeshQueryListIntersectionPoint(sphere_list_gel,3.5);
+  //gel_intersector2.buildIgfemMesh();
+  dumper_gel2_igfem.dump();
+  dumper_gel2_regular.dump();
+  nb_tri3_gel2 = mesh_gel2.getConnectivity(_triangle_3).getSize();
+  nb_tri4_gel2 = mesh_gel2.getConnectivity(_igfem_triangle_4).getSize();
+  nb_tri5_gel2 = mesh_gel2.getConnectivity(_igfem_triangle_5).getSize();
+  if ( (nb_tri3_gel2 != 1) || (nb_tri4_gel2 != 0) || (nb_tri5_gel2 != 1)){ 
+    std::cout << "final mesh with " << nb_tri3_gel2 << " _triangle_3, and " << nb_tri4_gel2
+	      << " _igfem_triangle_4, and " << nb_tri5_gel2 << " _igfem_triangle_5"<< std::endl; 
+    return EXIT_FAILURE;
+  }
 
   finalize();
   return EXIT_SUCCESS;
