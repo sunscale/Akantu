cdbedf2bc82004468a2ed5ca2ed79da510815ba0
Nicolas Richart <nicolas.richart@epfl.ch>
2016-01-22 16:37:23 +0100

    moved alejandro contact to a external repo

--- extra_packages/contact_alejandro/package.cmake
+++
@@ -1,9 +0,0 @@
-set(CMAKE_MODULE_PATH ${CMAKE_MODULE_PATH} "${CMAKE_CURRENT_LIST_DIR}/packages")
-
-include(contact)
-include(cpparray)
-include(nlopt)
-include(optimization)
-
-#add_example(contact              "Examples on how to use contact within Akantu"        PACKAGE contact)
-#add_example(optimization         "Optimization examples"                               PACKAGE optimization)
