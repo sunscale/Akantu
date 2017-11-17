911950510489ed4def06dd116ed2c698ca54116b
Guillaume Anciaux <guillaume.anciaux@epfl.ch>
2017-11-17 12:05:15 +0100

    material unit tests are ready

---
+++ test/test_model/test_solid_mechanics_model/test_materials/test_plastic_materials.cc
@@ -0,0 +1,35 @@
+/* -------------------------------------------------------------------------- */
+#include "material_linear_isotropic_hardening.hh"
+#include "solid_mechanics_model.hh"
+#include "test_material_fixtures.hh"
+#include <gtest/gtest.h>
+#include <type_traits>
+/* -------------------------------------------------------------------------- */
+
+using namespace akantu;
+
+using types = ::testing::Types<Traits<MaterialLinearIsotropicHardening, 1>,
+                               Traits<MaterialLinearIsotropicHardening, 2>,
+                               Traits<MaterialLinearIsotropicHardening, 3>>;
+
+/*****************************************************************/
+
+namespace {
+
+template <typename T>
+class TestPlasticMaterialFixture : public ::TestMaterialFixture<T> {};
+
+TYPED_TEST_CASE(TestPlasticMaterialFixture, types);
+
+TYPED_TEST(TestPlasticMaterialFixture, PlasticComputeStress) {
+  this->material->testComputeStress();
+}
+TYPED_TEST(TestPlasticMaterialFixture, PlasticEnergyDensity) {
+  this->material->testEnergyDensity();
+}
+TYPED_TEST(TestPlasticMaterialFixture, PlasticComputeTangentModuli) {
+  this->material->testComputeTangentModuli();
+}
+}
+
+/*****************************************************************/
