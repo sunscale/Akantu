1a545d12178cd0c37b61215410a1157a042ad6c2
Nicolas Richart <nicolas.richart@gmail.com>
2017-12-03 19:17:29 +0100

    Zip test as gtest

--- test/test_common/test_zip_iterator.cc
+++ test/test_common/test_zip_iterator.cc
@@ -29,7 +29,7 @@
 /* -------------------------------------------------------------------------- */
 #include "aka_iterators.hh"
 /* -------------------------------------------------------------------------- */
-#include <iostream>
+#include <gtest/gtest.h>
 #include <vector>
 /* -------------------------------------------------------------------------- */
 
@@ -39,10 +39,27 @@ template <class T> class A {
 public:
   A() = default;
   A(T a) : a(a){};
-  A(const A & other) : a(other.a), counter(other.counter + 1) {}
+  A(const A & other)
+      : a(other.a), copy_counter(other.copy_counter + 1),
+        move_counter(other.move_counter) {}
   A & operator=(const A & other) {
-    a = other.a;
-    counter = other.counter + 1;
+    if (this != &other) {
+      a = other.a;
+      copy_counter = other.copy_counter + 1;
+    }
+    return *this;
+  }
+
+  A(A && other)
+      : a(std::move(other.a)), copy_counter(std::move(other.copy_counter)),
+        move_counter(std::move(other.move_counter) + 1) {}
+
+  A & operator=(A && other) {
+    if (this != &other) {
+      a = std::move(other.a);
+      copy_counter = std::move(other.copy_counter);
+      move_counter = std::move(other.move_counter) + 1;
+    }
     return *this;
   }
 
@@ -50,45 +67,67 @@ public:
     a *= b;
     return *this;
   }
-  void to_stream(std::ostream & stream) const {
-    stream << a << " [" << counter << "]";
-  }
 
-private:
   T a;
-  size_t counter{0};
+  size_t copy_counter{0};
+  size_t move_counter{0};
 };
 
-template <class T>
-std::ostream & operator<<(std::ostream & stream, const A<T> & a) {
-  a.to_stream(stream);
-  return stream;
+class TestZipFixutre : public ::testing::Test {
+  void SetUp() override {
+    a.reserve(size);
+    b.reserve(size);
+
+    for (size_t i = 0; i < size; ++i) {
+      a.emplace_back(i);
+      b.emplace_back(i + size);
+    }
+  }
+
+protected:
+  size_t size{20};
+  std::vector<A<int>> a;
+  std::vector<A<float>> b;
+};
+
+TEST_F(TestZipFixutre, SimpleTest) {
+  size_t i = 0;
+  std::reference_wrapper<A<int>> a;
+  std::reference_wrapper<A<float>> b;
+  for (auto && pair : zip(a, b)) {
+    auto && a = std::get<0>(pair);
+    auto && b = std::get<1>(pair);
+
+    EXPECT_EQ(i, a.a);
+    EXPECT_EQ(0, a.copy_counter);
+    EXPECT_EQ(0, a.move_counter);
+
+    EXPECT_FLOAT_EQ(i + this->size, b.a);
+    EXPECT_EQ(0, b.copy_counter);
+    EXPECT_EQ(0, b.move_counter);
+    ++i;
+  }
 }
 
-/* -------------------------------------------------------------------------- */
-int main() {
-  std::vector<A<int>> a{1, 2, 3, 4, 5};
-  const std::vector<A<float>> b{6., 7., 8., 9., 10.};
+// /* --------------------------------------------------------------------------
+// */ int main() {
+//   auto ait = a.begin();
+//   auto bit = b.begin();
+//   auto aend = a.end();
+//   for (; ait != aend; ++ait, ++bit) {
 
-  auto aend = a.end();
+//   }
 
-  auto ait = a.begin();
-  auto bit = b.begin();
+//   for (auto pair : zip(a, b)) {
+//     std::cout << std::get<0>(pair) << " " << std::get<1>(pair) << std::endl;
+//     std::get<0>(pair) *= 10;
+//   }
 
-  for (; ait != aend; ++ait, ++bit) {
-    std::cout << *ait << " " << *bit << std::endl;
-  }
+//   ait = a.begin();
+//   bit = b.begin();
+//   for (; ait != aend; ++ait, ++bit) {
+//     std::cout << *ait << " " << *bit << std::endl;
+//   }
 
-  for (auto pair : zip(a, b)) {
-    std::cout << std::get<0>(pair) << " " << std::get<1>(pair) << std::endl;
-    std::get<0>(pair) *= 10;
-  }
-
-  ait = a.begin();
-  bit = b.begin();
-  for (; ait != aend; ++ait, ++bit) {
-    std::cout << *ait << " " << *bit << std::endl;
-  }
-
-  return 0;
-}
+//   return 0;
+// }
