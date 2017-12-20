ad0087629fd00068833a3eef1d85f82bb7c22037
Lucas Frérot <lucas.frerot@epfl.ch>
2017-12-20 16:12:55 +0100

    commented out output of shapes in FEEngineTemplate's printself

--- src/fe_engine/shape_functions.hh
+++ src/fe_engine/shape_functions.hh
@@ -63,8 +63,8 @@ public:
       ;
     stream << space << "Shapes [" << std::endl;
     integration_points.printself(stream, indent + 1);
-    shapes.printself(stream, indent + 1);
-    shapes_derivatives.printself(stream, indent + 1);
+    // shapes.printself(stream, indent + 1);
+    // shapes_derivatives.printself(stream, indent + 1);
     stream << space << "]" << std::endl;
   }
 
