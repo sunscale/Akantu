5dd58e262818e9888d7c00b7a594150f740a95ed
Guillaume Anciaux <guillaume.anciaux@epfl.ch>
2018-02-10 14:05:39 +0100

    adding missing files for heat transfer tests

---
+++ test/test_model/patch_tests/patch_test_linear_heat_transfer_implicit.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+from patch_test_linear_heat_transfer_fixture import TestPatchTestHTMLinear
+import akantu
+
+
+def foo(self):
+    self.initModel(akantu.HeatTransferModelOptions(akantu._implicit_dynamic),
+                   "heat_transfer_input.dat")
+
+    coordinates = self.mesh.getNodes()
+    temperature = self.model.getTemperature()
+    #  set the position of all nodes to the static solution
+    self.setLinearDOF(temperature, coordinates)
+
+    for s in range(0, 100):
+        self.model.solveStep()
+
+    self.checkAll()
+
+
+TestPatchTestHTMLinear.TYPED_TEST(foo, "Explicit")
