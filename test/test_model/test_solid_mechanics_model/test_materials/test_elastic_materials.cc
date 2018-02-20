2005839f7b04ac2f0829ac601f42fb3250a61b34
Enrico Milanese <enrico.milanese@epfl.ch>
2018-02-20 11:16:01 +0100

    celerity test for orthotropic and anisotropic materials

--- test/test_model/test_solid_mechanics_model/test_materials/test_elastic_materials.cc
+++ test/test_model/test_solid_mechanics_model/test_materials/test_elastic_materials.cc
@@ -14,11 +14,9 @@ using types =
     ::testing::Types<Traits<MaterialElastic, 1>, Traits<MaterialElastic, 2>,
                      Traits<MaterialElastic, 3>,
 
-                     Traits<MaterialElasticOrthotropic, 1>,
                      Traits<MaterialElasticOrthotropic, 2>,
                      Traits<MaterialElasticOrthotropic, 3>,
 
-                     Traits<MaterialElasticLinearAnisotropic, 1>,
                      Traits<MaterialElasticLinearAnisotropic, 2>,
                      Traits<MaterialElasticLinearAnisotropic, 3>>;
 
@@ -413,6 +411,59 @@ template <> void FriendMaterial<MaterialElasticOrthotropic<2>>::testComputeTange
 }
 
 /* -------------------------------------------------------------------------- */
+template <> void FriendMaterial<MaterialElasticOrthotropic<2>>::testPushWaveSpeed() {
+
+  // Note: for this test material and canonical basis coincide
+  Vector<Real> n1 = {1, 0};
+  Vector<Real> n2 = {0, 1};
+  Real E1 = 1.;
+  Real E2 = 2.;
+  Real nu12 = 0.1;
+  Real G12 = 2.;
+  Real rho = 2.5;
+
+  setParamNoUpdate("n1", n1);
+  setParamNoUpdate("n2", n2);
+  setParamNoUpdate("E1", E1);
+  setParamNoUpdate("E2", E2);
+  setParamNoUpdate("nu12", nu12);
+  setParamNoUpdate("G12", G12);
+  setParamNoUpdate("rho", rho);
+
+  // set internal Cijkl matrix expressed in the canonical frame of reference
+  this->updateInternalParameters();
+
+// construction of Cijkl engineering tensor in the *material* frame of reference
+// ref: http://solidmechanics.org/Text/Chapter3_2/Chapter3_2.php#Sect3_2_13
+  Real nu21 = nu12*E2/E1;
+  Real gamma = 1 / ( 1 - nu12*nu21 );
+
+  Matrix<Real> C_expected(3,3);
+  C_expected(0,0) = gamma * E1 ;
+  C_expected(1,1) = gamma * E2 ;
+  C_expected(2,2) = G12;
+
+  C_expected(1,0) = C_expected(0,1) = gamma * E1 * nu21;
+
+  Vector<Real> eig_expected(3);
+  C_expected.eig(eig_expected);
+
+  auto celerity_expected = std::sqrt(eig_expected(0)/rho);
+
+  auto celerity = this->getCelerity(Element());
+
+  ASSERT_NEAR(celerity_expected, celerity, 1e-14);
+}
+
+/* -------------------------------------------------------------------------- */
+template <> void FriendMaterial<MaterialElasticOrthotropic<2>>::testShearWaveSpeed() {
+
+  AKANTU_DEBUG_WARNING("WARNING: Shear wave speed for 2D orthotropic materials is not present in akantu."
+                       "Celerity is used instead to get the stable time step (see testPushWaveSpeed).");
+  ASSERT_NEAR(0, 0, 1e-14);
+}
+
+/* -------------------------------------------------------------------------- */
 template <> void FriendMaterial<MaterialElasticOrthotropic<3>>::testComputeStress() {
   UInt Dim = 3;
   Real E1 = 1.;
@@ -589,6 +640,80 @@ template <> void FriendMaterial<MaterialElasticOrthotropic<3>>::testComputeTange
 }
 
 /* -------------------------------------------------------------------------- */
+template <> void FriendMaterial<MaterialElasticOrthotropic<3>>::testPushWaveSpeed() {
+
+  // Note: for this test material and canonical basis coincide
+  UInt Dim = 3;
+  Vector<Real> n1 = {1, 0, 0};
+  Vector<Real> n2 = {0, 1, 0};
+  Vector<Real> n3 = {0, 0, 1};
+  Real E1 = 1.;
+  Real E2 = 2.;
+  Real E3 = 3.;
+  Real nu12 = 0.1;
+  Real nu13 = 0.2;
+  Real nu23 = 0.3;
+  Real G12 = 2.;
+  Real G13 = 3.;
+  Real G23 = 1.;
+  Real rho = 2.3;
+
+  setParamNoUpdate("n1", n1);
+  setParamNoUpdate("n2", n2);
+  setParamNoUpdate("n3", n3);
+  setParamNoUpdate("E1", E1);
+  setParamNoUpdate("E2", E2);
+  setParamNoUpdate("E3", E3);
+  setParamNoUpdate("nu12", nu12);
+  setParamNoUpdate("nu13", nu13);
+  setParamNoUpdate("nu23", nu23);
+  setParamNoUpdate("G12", G12);
+  setParamNoUpdate("G13", G13);
+  setParamNoUpdate("G23", G23);
+  setParamNoUpdate("rho", rho);
+
+  // set internal Cijkl matrix expressed in the canonical frame of reference
+  this->updateInternalParameters();
+
+// construction of Cijkl engineering tensor in the *material* frame of reference
+// ref: http://solidmechanics.org/Text/Chapter3_2/Chapter3_2.php#Sect3_2_13
+  Real nu21 = nu12*E2/E1;
+  Real nu31 = nu13*E3/E1;
+  Real nu32 = nu23*E3/E2;
+  Real gamma = 1 / ( 1 - nu12*nu21 - nu23*nu32 - nu31*nu13 - 2*nu21*nu32*nu13 );
+
+  Matrix<Real> C_expected(2*Dim,2*Dim,0);
+  C_expected(0,0) = gamma * E1 * ( 1-nu23*nu32 );
+  C_expected(1,1) = gamma * E2 * ( 1-nu13*nu31 );
+  C_expected(2,2) = gamma * E3 * ( 1-nu12*nu21 );
+
+  C_expected(1,0) = C_expected(0,1) = gamma * E1 * ( nu21 + nu31*nu23 );
+  C_expected(2,0) = C_expected(0,2) = gamma * E1 * ( nu31 + nu21*nu32 );
+  C_expected(2,1) = C_expected(1,2) = gamma * E2 * ( nu32 + nu12*nu31 );
+
+  C_expected(3,3) = G23;
+  C_expected(4,4) = G13;
+  C_expected(5,5) = G12;
+
+  Vector<Real> eig_expected(6);
+  C_expected.eig(eig_expected);
+
+  auto celerity_expected = std::sqrt(eig_expected(0)/rho);
+
+  auto celerity = this->getCelerity(Element());
+
+  ASSERT_NEAR(celerity_expected, celerity, 1e-14);
+}
+
+/* -------------------------------------------------------------------------- */
+template <> void FriendMaterial<MaterialElasticOrthotropic<3>>::testShearWaveSpeed() {
+
+  AKANTU_DEBUG_WARNING("WARNING: Shear wave speed for 3D orthotropic materials is not present in akantu."
+                       "Celerity is used instead to get the stable time step (see testPushWaveSpeed).");
+  ASSERT_NEAR(0, 0, 1e-14);
+}
+
+/* -------------------------------------------------------------------------- */
 template <> void FriendMaterial<MaterialElasticLinearAnisotropic<2>>::testComputeStress() {
   UInt Dim = 2;
   Matrix<Real> C = {
@@ -694,6 +819,47 @@ template <> void FriendMaterial<MaterialElasticLinearAnisotropic<2>>::testComput
 
   Real tangent_error = (tangent - C).norm<L_2>();
   ASSERT_NEAR(tangent_error, 0, 1e-14);
+}
+
+/* -------------------------------------------------------------------------- */
+template <> void FriendMaterial<MaterialElasticLinearAnisotropic<2>>::testPushWaveSpeed() {
+
+  // Note: for this test material and canonical basis coincide
+  Matrix<Real> C = {
+    {1.0, 0.3, 0.4},
+    {0.3, 2.0, 0.1},
+    {0.4, 0.1, 1.5},
+  };
+
+  Real rho = 2.7;
+
+  setParamNoUpdate("C11", C(0,0));
+  setParamNoUpdate("C12", C(0,1));
+  setParamNoUpdate("C13", C(0,2));
+  setParamNoUpdate("C22", C(1,1));
+  setParamNoUpdate("C23", C(1,2));
+  setParamNoUpdate("C33", C(2,2));
+  setParamNoUpdate("rho", rho);
+
+// set internal Cijkl matrix expressed in the canonical frame of reference
+  this->updateInternalParameters();
+
+  Vector<Real> eig_expected(3);
+  C.eig(eig_expected);
+
+  auto celerity_expected = std::sqrt(eig_expected(0)/rho);
+
+  auto celerity = this->getCelerity(Element());
+
+  ASSERT_NEAR(celerity_expected, celerity, 1e-14);
+}
+
+/* -------------------------------------------------------------------------- */
+template <> void FriendMaterial<MaterialElasticLinearAnisotropic<2>>::testShearWaveSpeed() {
+
+  AKANTU_DEBUG_WARNING("WARNING: Shear wave speed for 2D anisotropic materials is not present in akantu."
+                       "Celerity is used instead to get the stable time step (see testPushWaveSpeed).");
+  ASSERT_NEAR(0, 0, 1e-14);
 }
 
 /* -------------------------------------------------------------------------- */
@@ -847,6 +1013,64 @@ template <> void FriendMaterial<MaterialElasticLinearAnisotropic<3>>::testComput
 
   Real tangent_error = (tangent - C).norm<L_2>();
   ASSERT_NEAR(tangent_error, 0, 1e-14);
+}
+
+/* -------------------------------------------------------------------------- */
+template <> void FriendMaterial<MaterialElasticLinearAnisotropic<3>>::testPushWaveSpeed() {
+
+  // Note: for this test material and canonical basis coincide
+  Matrix<Real> C = {
+    {1.0, 0.3, 0.4, 0.3, 0.2, 0.1},
+    {0.3, 2.0, 0.1, 0.2, 0.3, 0.2},
+    {0.4, 0.1, 1.5, 0.1, 0.4, 0.3},
+    {0.3, 0.2, 0.1, 2.4, 0.1, 0.4},
+    {0.2, 0.3, 0.4, 0.1, 0.9, 0.1},
+    {0.1, 0.2, 0.3, 0.4, 0.1, 1.2},
+  };
+  Real rho = 2.9;
+
+  setParamNoUpdate("C11", C(0,0));
+  setParamNoUpdate("C12", C(0,1));
+  setParamNoUpdate("C13", C(0,2));
+  setParamNoUpdate("C14", C(0,3));
+  setParamNoUpdate("C15", C(0,4));
+  setParamNoUpdate("C16", C(0,5));
+  setParamNoUpdate("C22", C(1,1));
+  setParamNoUpdate("C23", C(1,2));
+  setParamNoUpdate("C24", C(1,3));
+  setParamNoUpdate("C25", C(1,4));
+  setParamNoUpdate("C26", C(1,5));
+  setParamNoUpdate("C33", C(2,2));
+  setParamNoUpdate("C34", C(2,3));
+  setParamNoUpdate("C35", C(2,4));
+  setParamNoUpdate("C36", C(2,5));
+  setParamNoUpdate("C44", C(3,3));
+  setParamNoUpdate("C45", C(3,4));
+  setParamNoUpdate("C46", C(3,5));
+  setParamNoUpdate("C55", C(4,4));
+  setParamNoUpdate("C56", C(4,5));
+  setParamNoUpdate("C66", C(5,5));
+  setParamNoUpdate("rho", rho);
+
+  // set internal Cijkl matrix expressed in the canonical frame of reference
+  this->updateInternalParameters();
+
+  Vector<Real> eig_expected(6);
+  C.eig(eig_expected);
+
+  auto celerity_expected = std::sqrt(eig_expected(0)/rho);
+
+  auto celerity = this->getCelerity(Element());
+
+  ASSERT_NEAR(celerity_expected, celerity, 1e-14);
+}
+
+/* -------------------------------------------------------------------------- */
+template <> void FriendMaterial<MaterialElasticLinearAnisotropic<3>>::testShearWaveSpeed() {
+
+  AKANTU_DEBUG_WARNING("WARNING: Shear wave speed for 3D anisotropic materials is not present in akantu."
+                       "Celerity is used instead to get the stable time step (see testPushWaveSpeed).");
+  ASSERT_NEAR(0, 0, 1e-14);
 }
 
 /* -------------------------------------------------------------------------- */
