195c4be2fa7ad27379c642be921a602db08ffd65
Nicolas Richart <nicolas.richart@epfl.ch>
2018-11-06 17:02:36 +0100

    try more advances debug flags

--- cmake/AkantuExtraCompilationProfiles.cmake
+++ cmake/AkantuExtraCompilationProfiles.cmake
@@ -1,9 +1,9 @@
 #Profiling
-set(CMAKE_CXX_FLAGS_PROFILING "-g -pg -DNDEBUG -DAKANTU_NDEBUG -O2"
+set(CMAKE_CXX_FLAGS_PROFILING "-g -gdb3 -pg -DNDEBUG -DAKANTU_NDEBUG -O2"
   CACHE STRING "Flags used by the compiler during profiling builds")
-set(CMAKE_C_FLAGS_PROFILING "-g -pg -DNDEBUG -DAKANTU_NDEBUG -O2"
+set(CMAKE_C_FLAGS_PROFILING "-g -gdb3 -pg -DNDEBUG -DAKANTU_NDEBUG -O2"
   CACHE STRING "Flags used by the compiler during profiling builds")
-set(CMAKE_Fortran_FLAGS_PROFILING "-g -pg -DNDEBUG -DAKANTU_NDEBUG -O2"
+set(CMAKE_Fortran_FLAGS_PROFILING "-g -gdb3 -pg -DNDEBUG -DAKANTU_NDEBUG -O2"
   CACHE STRING "Flags used by the compiler during profiling builds")
 set(CMAKE_EXE_LINKER_FLAGS_PROFILING "-pg"
   CACHE STRING "Flags used by the linker during profiling builds")
@@ -14,11 +14,11 @@ set(CMAKE_SHARED_LINKER_FLAGS_PROFILING "-pg"
 # Sanitize the code
 if ((CMAKE_CXX_COMPILER_ID STREQUAL "GNU" AND CMAKE_CXX_COMPILER_VERSION VERSION_GREATER "5.2") OR
     CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
-  set(CMAKE_CXX_FLAGS_SANITIZE "-g -O2 -fsanitize=address -fsanitize=leak -fsanitize=undefined -fno-omit-frame-pointer"
+  set(CMAKE_CXX_FLAGS_SANITIZE "-g -gdb3 -O2 -fsanitize=address -fsanitize=leak -fsanitize=undefined -fno-omit-frame-pointer"
     CACHE STRING "Flags used by the compiler during sanitining builds")
-  set(CMAKE_C_FLAGS_SANITIZE "-g -O2 -fsanitize=address -fsanitize=leak -fsanitize=undefined -fno-omit-frame-pointer"
+  set(CMAKE_C_FLAGS_SANITIZE "-g -gdb3 -O2 -fsanitize=address -fsanitize=leak -fsanitize=undefined -fno-omit-frame-pointer"
     CACHE STRING "Flags used by the compiler during sanitining builds")
-  set(CMAKE_Fortran_FLAGS_SANITIZE "-g -O2 -fsanitize=address -fsanitize=leak -fsanitize=undefined -fno-omit-frame-pointer"
+  set(CMAKE_Fortran_FLAGS_SANITIZE "-g -gdb3 -O2 -fsanitize=address -fsanitize=leak -fsanitize=undefined -fno-omit-frame-pointer"
     CACHE STRING "Flags used by the compiler during sanitining builds")
   set(CMAKE_EXE_LINKER_FLAGS_SANITIZE "-fsanitize=address -fsanitize=leak -fsanitize=undefined -fno-omit-frame-pointer"
     CACHE STRING "Flags used by the linker during sanitining builds")
