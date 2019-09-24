9b0b8a6b22b22130678238035719f57493270a4b
Nicolas Richart <nicolas.richart@epfl.ch>
2019-09-24 10:56:52 +0200

    preparation to split hbm scripts in a different project

--- harbomaster/__init__.py
+++ harbomaster/__init__.py
@@ -1,3 +1,14 @@
+# ------------------------------------------------------------------------------
+__author__ = "Nicolas Richart"
+__copyright__ = "Copyright (C) 2019, EPFL (Ecole Polytechnique Fédérale" \
+                " de Lausanne) Laboratory (LSMS - Laboratoire de Simulation" \
+                " en Mécanique des Solides)"
+__credits__ = ["Nicolas Richart", "Lucas Frerot"]
+__license__ = "L-GPLv3"
+__maintainer__ = "Nicolas Richart"
+__email__ = "nicolas.richart@epfl.ch"
+# ------------------------------------------------------------------------------
+
 # for the module
 import sys as __hbm_sys
 
