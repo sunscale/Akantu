33b2a21d75fae3c107381681019a1ef94e710f3f
Guillaume Anciaux <guillaume.anciaux@epfl.ch>
2019-11-06 14:20:46 +0100

    adding possibilities for the element/node groups

--- python/py_group_manager.cc
+++ python/py_group_manager.cc
@@ -30,7 +30,13 @@ void register_group_manager(py::module & mod) {
             return self.getNodeGroup();
           },
           py::return_value_policy::reference)
-      .def("getName", &ElementGroup::getName);
+      .def("getName", &ElementGroup::getName)
+      .def(
+          "getElements",
+          [](ElementGroup & self) -> decltype(auto) {
+            return self.getElements();
+          },
+          py::return_value_policy::reference);
 
   /* ------------------------------------------------------------------------ */
   py::class_<GroupManager>(mod, "GroupManager")
@@ -73,22 +79,26 @@ void register_group_manager(py::module & mod) {
             return self.getNodeGroup(name);
           },
           py::return_value_policy::reference)
-      .def("nodeGroups",
-           [](GroupManager & self) {
-             std::vector<NodeGroup*> groups;
-             for (auto & g : self.iterateNodeGroups()) {
-               groups.push_back(&g);
-             }
-             return groups;
-           }, py::return_value_policy::reference)
-      .def("elementGroups",
-           [](GroupManager & self) {
-             std::vector<ElementGroup*> groups;
-             for (auto & g : self.iterateElementGroups()) {
-               groups.push_back(&g);
-             }
-             return groups;
-           }, py::return_value_policy::reference)
+      .def(
+          "nodeGroups",
+          [](GroupManager & self) {
+            std::vector<NodeGroup *> groups;
+            for (auto & g : self.iterateNodeGroups()) {
+              groups.push_back(&g);
+            }
+            return groups;
+          },
+          py::return_value_policy::reference)
+      .def(
+          "elementGroups",
+          [](GroupManager & self) {
+            std::vector<ElementGroup *> groups;
+            for (auto & g : self.iterateElementGroups()) {
+              groups.push_back(&g);
+            }
+            return groups;
+          },
+          py::return_value_policy::reference)
       .def("createBoundaryGroupFromGeometry",
            &GroupManager::createBoundaryGroupFromGeometry);
 }
