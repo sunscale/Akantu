520fbdda217ecd848ed197f1b1f4c45e497eae41
Nicolas Richart <nicolas.richart@epfl.ch>
2019-11-11 16:03:16 +0100

    Adding finite deformation materials to patch tests

--- test/test_model/patch_tests/patch_test_linear_elastic_explicit.cc
+++ test/test_model/patch_tests/patch_test_linear_elastic_explicit.cc
@@ -54,3 +54,28 @@ TYPED_TEST(TestPatchTestSMMLinear, Explicit) {
 
   this->checkAll();
 }
+
+/* -------------------------------------------------------------------------- */
+TYPED_TEST(TestPatchTestSMMLinear, ExplicitFiniteDeformation) {
+  std::string filename = "material_check_stress_plane_stress_finite_deformation.dat";
+  if (this->plane_strain)
+    filename = "material_check_stress_plane_strain_finite_deformation.dat";
+
+  this->initModel(_explicit_lumped_mass, filename);
+
+  const auto & coordinates = this->mesh->getNodes();
+  auto & displacement = this->model->getDisplacement();
+  // set the position of all nodes to the static solution
+  for (auto && tuple : zip(make_view(coordinates, this->dim),
+                           make_view(displacement, this->dim))) {
+    this->setLinearDOF(std::get<1>(tuple), std::get<0>(tuple));
+  }
+  for (UInt s = 0; s < 100; ++s) {
+    this->model->solveStep();
+  }
+
+  auto ekin = this->model->getEnergy("kinetic");
+  EXPECT_NEAR(0, ekin, 1e-16);
+
+  this->checkAll();
+}
