d5726d36cee232a5fe800d1f800e94c6507240de
Nicolas Richart <nicolas.richart@epfl.ch>
2019-12-04 21:08:02 +0100

    do not buildFacets in between constructor and initFull...

--- src/model/solid_mechanics/solid_mechanics_model.cc
+++ src/model/solid_mechanics/solid_mechanics_model.cc
@@ -711,7 +711,6 @@ void SolidMechanicsModel::onElementsAdded(const Array<Element> & element_list,
   ElementTypeMapArray<UInt> filter("new_element_filter", this->getID(),
                                    this->getMemoryID());
 
-  UInt count = 0;
   for (auto & elem : element_list) {
     if (mesh.getSpatialDimension(elem.type) != spatial_dimension)
       continue;
@@ -719,12 +718,11 @@ void SolidMechanicsModel::onElementsAdded(const Array<Element> & element_list,
     if (!filter.exists(elem.type, elem.ghost_type))
       filter.alloc(0, 1, elem.type, elem.ghost_type);
     filter(elem.type, elem.ghost_type).push_back(elem.element);
-
-    ++count;
   }
 
-  if (count != 0)
-    this->assignMaterialToElements(&filter);
+  // this fails in parallel if the event is sent on facet between constructor
+  // and initFull \todo: to debug...
+  this->assignMaterialToElements(&filter);
 
   for (auto & material : materials)
     material->onElementsAdded(element_list, event);
