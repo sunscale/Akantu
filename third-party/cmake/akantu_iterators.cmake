38689942d5bb52efc26f1ee5d6ae76be99e62566
Nicolas Richart <nicolas.richart@epfl.ch>
2019-12-18 14:45:32 +0100

    Bug fix in the export of the target akantu

--- third-party/cmake/akantu_iterators.cmake
+++ third-party/cmake/akantu_iterators.cmake
@@ -4,3 +4,4 @@ add_subdirectory(${PROJECT_SOURCE_DIR}/third-party/akantu_iterators)
 
 set(AKANTU_ITERATORS_VERSION "master" CACHE INTERNAL "")
 set(AKANTU_ITERATORS_LIBRARIES "akantu_iterators" CACHE INTERNAL "")
+package_add_to_export_list(akantu_iterators akantu_iterators)
