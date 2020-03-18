44a04848e239191094885a817d73f943251f3962
Guillaume Anciaux <guillaume.anciaux@epfl.ch>
2020-03-18 16:17:19 +0100

    allow to initialize the material manually

--- src/model/solid_mechanics/solid_mechanics_model_material.cc
+++ src/model/solid_mechanics/solid_mechanics_model_material.cc
@@ -143,13 +143,14 @@ void SolidMechanicsModel::assignMaterialToElements(
   if (non_local_manager)
     non_local_manager->synchronize(*this, SynchronizationTag::_material_id);
 
-  for_each_element(mesh,
-                   [&](auto && element) {
-                     auto mat_index = material_index(element);
-                     auto index = materials[mat_index]->addElement(element);
-                     material_local_numbering(element) = index;
-                   },
-                   _element_filter = filter, _ghost_type = _not_ghost);
+  for_each_element(
+      mesh,
+      [&](auto && element) {
+        auto mat_index = material_index(element);
+        auto index = materials[mat_index]->addElement(element);
+        material_local_numbering(element) = index;
+      },
+      _element_filter = filter, _ghost_type = _not_ghost);
 
   // synchronize the element material arrays
   this->synchronize(SynchronizationTag::_material_id);
@@ -159,8 +160,8 @@ void SolidMechanicsModel::assignMaterialToElements(
 void SolidMechanicsModel::initMaterials() {
   AKANTU_DEBUG_ASSERT(materials.size() != 0, "No material to initialize !");
 
-  if (!are_materials_instantiated)
-    instantiateMaterials();
+  // if (!are_materials_instantiated)
+  //   instantiateMaterials();
 
   this->assignMaterialToElements();
 
