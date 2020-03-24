835ec6cebe6a28ed60ce942c6bf7a6e0ffe79920
Nicolas Richart <nicolas.richart@epfl.ch>
2020-03-24 11:23:15 +0100

    Deactivating the copy as target, copy at configure instead

--- cmake/AkantuMacros.cmake
+++ cmake/AkantuMacros.cmake
@@ -45,19 +45,24 @@ function(_add_file_to_copy target file)
   get_filename_component(_file_path ${file}
     ABSOLUTE BASE_DIR ${CMAKE_CURRENT_SOURCE_DIR})
 
-  set(copy_target copy_${_file_name_we}_${_file_name_ext}_${target})
-  add_custom_target(${copy_target}
-    DEPENDS ${CMAKE_CURRENT_BINARY_DIR}/${_file_name})
-  add_custom_command(
-    OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/${_file_name}
-    COMMAND ${CMAKE_COMMAND} -E copy_if_different
-                ${file}
-		${CMAKE_CURRENT_BINARY_DIR}
-    WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
-    DEPENDS ${_file_path}
-    COMMENT "Copying file ${_file_name} for the target ${target}"
-    )
-  add_dependencies(${target} ${copy_target})
+  configure_file(
+    ${_file_path}
+    ${CMAKE_CURRENT_BINARY_DIR}/${_file_name}
+    COPYONLY)
+
+  # set(copy_target copy_${_file_name_we}_${_file_name_ext}_${target})
+  # add_custom_target(${copy_target}
+  #   DEPENDS ${CMAKE_CURRENT_BINARY_DIR}/${_file_name})
+  # add_custom_command(
+  #   OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/${_file_name}
+  #   COMMAND ${CMAKE_COMMAND} -E copy_if_different
+  #               ${file}
+	# 	${CMAKE_CURRENT_BINARY_DIR}
+  #   WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+  #   DEPENDS ${_file_path}
+  #   COMMENT "Copying file ${_file_name} for the target ${target}"
+  #   )
+  # add_dependencies(${target} ${copy_target})
 endfunction()
 
 # ==============================================================================
