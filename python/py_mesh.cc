4ba9503a3f37da022fc325ebf0a1c4d9c4314229
Philip Mueller <philip.paul.mueller@bluemain.ch>
2021-02-04 16:33:39 +0100

    This commit fixes an error, at least I think that, in the Python interface of Akantu.
    The problem is that several parts needs the `ElementTypeMapArray` type, such as the mesh, the FEEngine and the material, probably even more, but grep is not able to find them (because of the use of `auto`).
    This type is indirectly registered by the `register_material()` function in the `py_material.cc` file.
    However that function is only run if the solid mechanics part is enabled.
    Since this is almost always the case, it was probably not an issue till now.
    However since I have deactivated it, for my development of the structural mechanics part, I run into that issue, there is still one, but I have not found it, I am not even sure if _this_ is an error.
    
    The fix is that the `ElementTypeMapArray` type is now registered by the `register_mesh()` function, which is the first occurrence of that type (as far as I can tell) and is always built.
    
    An important side note is, that the `InternalField` is sill registered by the material.

--- python/py_mesh.cc
+++ python/py_mesh.cc
@@ -13,8 +13,43 @@ namespace py = pybind11;
 
 namespace akantu {
 
+
+/* -------------------------------------------------------------------------- */
+template <typename T>
+void register_element_type_map_array(py::module & mod,
+                                     const std::string & name) {
+  py::class_<ElementTypeMapArray<T>, std::shared_ptr<ElementTypeMapArray<T>>>(
+      mod, ("ElementTypeMapArray" + name).c_str())
+      .def(
+          "__call__",
+          [](ElementTypeMapArray<T> & self, ElementType type,
+             GhostType ghost_type) -> decltype(auto) {
+            return self(type, ghost_type);
+          },
+          py::arg("type"), py::arg("ghost_type") = _not_ghost,
+          py::return_value_policy::reference)
+      .def(
+          "elementTypes",
+          [](ElementTypeMapArray<T> & self, UInt _dim, GhostType _ghost_type,
+             ElementKind _kind) -> decltype(auto) {
+            auto types = self.elementTypes(_dim, _ghost_type, _kind);
+            std::vector<ElementType> _types;
+            for (auto && t : types) {
+              _types.push_back(t);
+            }
+            return _types;
+          },
+          py::arg("dim") = _all_dimensions, py::arg("ghost_type") = _not_ghost,
+          py::arg("kind") = _ek_regular);
+}
+
+
 /* -------------------------------------------------------------------------- */
 void register_mesh(py::module & mod) {
+
+  register_element_type_map_array<Real>(mod, "Real");
+  register_element_type_map_array<UInt>(mod, "UInt");
+
   py::class_<MeshData>(mod, "MeshData")
       .def(
           "getElementalDataUInt",
