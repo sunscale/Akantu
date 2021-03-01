c41f89ae02c797f7933345911fa45d0d2b61e0fc
Mohit Pundir <mohit.pundir@epfl.ch>
2021-03-02 00:57:55 +0100

    Bug Fixed : added definition of computeNtbN function for structural class

--- src/fe_engine/shape_structural.hh
+++ src/fe_engine/shape_structural.hh
@@ -152,6 +152,14 @@ public:
     AKANTU_TO_IMPLEMENT();
   }
 
+  
+  template <ElementType type>
+  void computeNtbN(const Array<Real> & /*bs*/, Array<Real> & /*NtbNs*/,
+                   UInt /*order_d*/, GhostType /*ghost_type*/,
+                   const Array<UInt> & /*filter_elements*/) const {
+    AKANTU_TO_IMPLEMENT();
+  }
+
   /// multiply a field by shape functions
   template <ElementType type>
   void
