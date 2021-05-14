5c0a955c98b932d561bfffe81531d5f2dbe35a87
Nicolas Richart <nicolas.richart@epfl.ch>
2021-05-14 10:13:23 +0200

    Correcting compilation errors on newer version of pybind11

--- python/py_phase_field_model.hh
+++ python/py_phase_field_model.hh
@@ -1,9 +1,7 @@
+#include <pybind11/pybind11.h>
+
 #ifndef __AKANTU_PY_PHASE_FIELD_MODEL_HH__
 #define __AKANTU_PY_PHASE_FIELD_MODEL_HH__
-
-namespace pybind11 {
-  struct module;
-}//namespace pybind11
 
 namespace akantu {
   void register_phase_field_model(pybind11::module & mod);
